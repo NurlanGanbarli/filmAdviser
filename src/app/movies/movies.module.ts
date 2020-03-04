9ba51a3c8e86b28a206e99cf82168520d322a373
Nurlan Qenberli <nurlan.qenberli.94@gmail.com>
2020-03-04 16:34:59 +0400

    create header component and export from movie module for app module

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesListComponent } from './components/movies-list/movies-list.component';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
-  declarations: [MoviesListComponent, MovieDetailComponent],
+  declarations: [MoviesListComponent, MovieDetailComponent, HeaderComponent],
   imports: [
     CommonModule,
     MoviesRoutingModule
+  ],
+  exports: [
+    MoviesListComponent,
+    MovieDetailComponent,
+    HeaderComponent
   ]
 })
 export class MoviesModule { }
